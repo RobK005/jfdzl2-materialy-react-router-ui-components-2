5e6bc1a3b01316c6c040d4fa09e5051f5f4ebf6a
Patryk Omiotek <patryk.omiotek@gmail.com>
2019-02-09 11:19:32 +0100

    refactored TasksContainer

--- apps/todo/src/App.js
+++ apps/todo/src/App.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 
 import TasksContainer from './TaskContainer';
 class App extends Component {
+
+  state = {
+    day: 'sobota',
+    tasks: [
+      { id: 95, name: 'Odkurzyc', completed: false },
+      { id: 24, name: 'Zakupy', completed: true },
+      { id: 542, name: 'Nauczyć się Reacta', completed: false }
+    ]
+  };
+
+  handleChange = (event, taskId) => {
+    console.log('mam taskId', taskId);
+    const taskIndex = this.state.tasks.findIndex(task => task.id === taskId);
+    const task = { ...this.state.tasks[taskIndex] };
+
+    task.completed = event.target.checked;
+
+    const tasks = [...this.state.tasks];
+    tasks[taskIndex] = task;
+
+    this.setState({ tasks: tasks });  // this.setState({ tasks });
+  }
+
   render() {
     return (
       <div>
-        <TasksContainer />
+        <TasksContainer
+          todos={this.state.tasks}
+          handleChange={this.handleChange}
+        />
       </div>
     );
   }
--- apps/todo/src/TaskContainer.js
+++ apps/todo/src/TaskContainer.js
@@ -4,35 +4,14 @@ import Task from './Task';
 
 class TaskContainer extends Component {
 
-  state = {
-    day: 'sobota',
-    tasks: [
-      { id: 95, name: 'Odkurzyc', completed: false },
-      { id: 24, name: 'Zakupy', completed: true },
-      { id: 542, name: 'Nauczyć się Reacta', completed: false }
-    ]
-  };
-
-  handleChange = (event, taskId) => {
-    const taskIndex = this.state.tasks.findIndex(task => task.id === taskId);
-    const task = { ...this.state.tasks[taskIndex] };
-
-    task.completed = event.target.checked;
-
-    const tasks = [...this.state.tasks];
-    tasks[taskIndex] = task;
-
-    this.setState({ tasks: tasks });  // this.setState({ tasks });
-  }
-
   render() {
     return (
       <div>
-        {this.state.tasks.map(task => (
+        {this.props.todos.map(task => (
           <Task
             key={`t-${task.id}`}
             task={task}
-            handleChange={(event) => this.handleChange(event, task.id)}
+            handleChange={(event) => this.props.handleChange(event, task.id)}
           />
         ))}
       </div>
